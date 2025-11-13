2bf214ee2ac7a0bb3c2d2fb088285c99ed347470
openvault <157576678+mazharmecci@users.noreply.github.com>
2025-11-13 10:59:32 +0530

    Create firebase.js

---
+++ firebase.js
@@ -0,0 +1,22 @@
+// Firebase SDK imports
+import { initializeApp } from "https://www.gstatic.com/firebasejs/10.5.0/firebase-app.js";
+import { getAuth } from "https://www.gstatic.com/firebasejs/10.5.0/firebase-auth.js";
+import { getFirestore } from "https://www.gstatic.com/firebasejs/10.5.0/firebase-firestore.js";
+
+// Firebase config
+const firebaseConfig = {
+  apiKey: "AIzaSyC-heT_dKo-O6523VVNPKW_Q-D6qxW8_HY",
+  authDomain: "xpay-b0298.firebaseapp.com",
+  projectId: "xpay-b0298",
+  storageBucket: "xpay-b0298.firebasestorage.app",
+  messagingSenderId: "518708112275",
+  appId: "1:518708112275:web:0ea8d94b4c3734d1895666"
+};
+
+// Initialize Firebase
+const app = initializeApp(firebaseConfig);
+const auth = getAuth(app);
+const db = getFirestore(app);
+
+// Export for use in other files
+export { auth, db };
