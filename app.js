3b6898835cfb899f3a6447afac37456d0605d6c0
openvault <157576678+mazharmecci@users.noreply.github.com>
2025-11-13 10:59:11 +0530

    Update app.js

--- app.js
+++ app.js
@@ -1,3 +1,35 @@
+
+import { auth, db } from './firebase.js';
+import { signInWithEmailAndPassword } from "https://www.gstatic.com/firebasejs/10.5.0/firebase-auth.js";
+import { doc, getDoc } from "https://www.gstatic.com/firebasejs/10.5.0/firebase-firestore.js";
+
+const loginForm = document.getElementById('loginForm');
+loginForm.addEventListener('submit', async (e) => {
+  e.preventDefault();
+  const email = email.value;
+  const password = password.value;
+
+  try {
+    const userCredential = await signInWithEmailAndPassword(auth, email, password);
+    const uid = userCredential.user.uid;
+
+    const userDoc = await getDoc(doc(db, "users", uid));
+    const role = userDoc.data().role;
+
+    if (role === 'employee') {
+      window.location.href = 'employee.html';
+    } else if (role === 'accountant') {
+      window.location.href = 'accountant.html';
+    } else if (role === 'manager') {
+      window.location.href = 'manager.html';
+    } else {
+      alert("Role not assigned. Contact admin.");
+    }
+  } catch (error) {
+    alert(error.message);
+  }
+});
+
 document.getElementById('expenseForm').addEventListener('submit', function(e) {
   e.preventDefault();
   alert("Expense submitted successfully!"); 
